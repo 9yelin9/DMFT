fe751a6986e58811793b112815194ebd12b55c70
9yelin9 <9yelin9@gmail.com>
2021-04-02 20:47:32 +0900

    Update

---
+++ dmft.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -q single.q
+#$ -v OMP_NUM_THREADS=1
+#$ -cwd
+#$ -j y
+
+pwd
+echo "# time-start "`date`
+echo "--------------------------------------------"
+##source ~/.bashrc
+./hamiltonian
+echo "--------------------------------------------"
+~
+
---
+++ hamiltonian.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main() {
+	int i;
+
+	
+
+	return 0;
+}
